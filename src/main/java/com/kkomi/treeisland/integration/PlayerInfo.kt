80c6e2a430ca8992f7c601e9c71495ec0db79ece
꼬미 <reona7140@naver.com>
2019-11-21 23:32:17 +0900

    Add SendMessage Function

--- src/main/java/com/kkomi/treeisland/integration/PlayerInfo.kt
+++ src/main/java/com/kkomi/treeisland/integration/PlayerInfo.kt
@@ -1,5 +1,8 @@
 package com.kkomi.treeisland.integration
 
+import com.kkomi.library.extension.sendDebugMessage
+import com.kkomi.library.extension.sendErrorMessage
+import com.kkomi.library.extension.sendInfoMessage
 import com.kkomi.treeisland.money.MoneyPlugin
 import com.kkomi.treeisland.money.entity.PlayerMoney
 import org.bukkit.entity.Player
@@ -9,4 +12,14 @@ class PlayerInfo(
 ) {
     private val uuid : String = player.uniqueId.toString()
     val moneyInfo : PlayerMoney = MoneyPlugin.moneyManager.getPlayerMoney(uuid)!!
+
+    fun sendErrorMessage(message : Any) {
+        player.sendErrorMessage(message)
+    }
+    fun sendInfoMessage(message : Any) {
+        player.sendInfoMessage(message)
+    }
+    fun sendDebugMessage(message : Any) {
+        player.sendDebugMessage(message)
+    }
 }
