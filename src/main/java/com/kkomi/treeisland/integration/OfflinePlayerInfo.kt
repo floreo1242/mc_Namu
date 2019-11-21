c1b3232ea0bd3a5e539013fbc6bb1892c54ec610
꼬미 <reona7140@naver.com>
2019-11-21 23:31:59 +0900

    Remove Log Function

--- src/main/java/com/kkomi/treeisland/integration/OfflinePlayerInfo.kt
+++ src/main/java/com/kkomi/treeisland/integration/OfflinePlayerInfo.kt
@@ -3,7 +3,6 @@ package com.kkomi.treeisland.integration
 import com.kkomi.treeisland.money.MoneyPlugin
 import com.kkomi.treeisland.money.entity.PlayerMoney
 import org.bukkit.OfflinePlayer
-import org.bukkit.entity.Player
 
 class OfflinePlayerInfo(
         val offlinePlayer: OfflinePlayer
@@ -11,8 +10,6 @@ class OfflinePlayerInfo(
     private val uuid: String = offlinePlayer.uniqueId.toString()
     val moneyInfo: PlayerMoney
         get() {
-            println(uuid)
-            println(MoneyPlugin.moneyManager.uuids)
             return MoneyPlugin.moneyManager.getPlayerMoney(uuid)!!
         }
 }
