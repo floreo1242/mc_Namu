f052eb1cf73b97fb5d59373c38a72fd6cff261fe
꼬미 <reona7140@naver.com>
2019-12-25 21:29:42 +0900

    이미 존재하는 상점이 중복 생성되던 버그 수정

--- src/main/java/com/kkomi/treeisland/plugin/shop/command/CommandCreateShop.kt
+++ src/main/java/com/kkomi/treeisland/plugin/shop/command/CommandCreateShop.kt
@@ -2,6 +2,7 @@ package com.kkomi.treeisland.plugin.shop.command
 
 import com.kkomi.treeisland.library.command.ArgumentList
 import com.kkomi.treeisland.library.command.CommandComponent
+import com.kkomi.treeisland.library.extension.sendErrorMessage
 import com.kkomi.treeisland.library.extension.sendInfoMessage
 import com.kkomi.treeisland.plugin.shop.ShopPlugin
 import com.kkomi.treeisland.plugin.shop.model.ShopMessage
@@ -13,7 +14,12 @@ import org.bukkit.entity.Player
 
 class CommandCreateShop(usage: String, description: String, argumentsLength: Int) : CommandComponent(usage, description, argumentsLength) {
     override fun onCommand(sender: CommandSender, label: String, command: Command, componentLabel: String, args: ArgumentList): Boolean {
-        ShopRepository.addShop(Shop(args.next(), mutableListOf(), ""))
+        val shopName = args.next()
+        if (ShopRepository.getShop(shopName) != null) {
+            sender.sendErrorMessage(ShopMessage.ALREADY_SHOP)
+            return true
+        }
+        ShopRepository.addShop(Shop(shopName, mutableListOf(), ""))
         (sender as Player).sendInfoMessage(ShopMessage.CREATE_SHOP)
         return true
     }
