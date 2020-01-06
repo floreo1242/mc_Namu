b35dd6249d1efdad028e930b022c8d242839f729
꼬미 <reona7140@naver.com>
2020-01-06 20:41:29 +0900

    퀘스트 플러그인 커맨드 대량 삭제

--- src/main/java/com/kkomi/treeisland/plugin/quest/command/CommandQuestRemove.kt
+++ src/main/java/com/kkomi/treeisland/plugin/quest/command/CommandQuestRemove.kt
@@ -1,6 +1,7 @@
 package com.kkomi.treeisland.plugin.quest.command
 
 import com.kkomi.treeisland.library.command.ArgumentList
+import com.kkomi.treeisland.library.extension.sendErrorMessage
 import com.kkomi.treeisland.library.extension.sendInfoMessage
 import com.kkomi.treeisland.plugin.quest.model.QuestMessage
 import com.kkomi.treeisland.plugin.quest.model.QuestRepository
@@ -9,13 +10,11 @@ import org.bukkit.command.Command
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
 
-class CommandSetQuestDisposeMessage(usage: String, description: String, argumentsLength: Int) : QuestCommandComponent(usage, description, argumentsLength) {
+class CommandQuestRemove(usage: String, description: String, argumentsLength: Int) : QuestCommandComponent(usage, description, argumentsLength) {
 
     override fun onCommand(commandSender: CommandSender, label: String, command: Command, componentLabel: String, args: ArgumentList, quest: Quest): Boolean {
-        val disposeMessage = args.join(" ")
-        quest.disposeMessage = disposeMessage
-        QuestRepository.editQuest(quest)
-        commandSender.sendInfoMessage(QuestMessage.QUEST_SET_DISPOSE_MESSAGE)
+        QuestRepository.removeQuest(quest.name)
+        commandSender.sendInfoMessage(QuestMessage.QUEST_DELETE)
         return true
     }
 
