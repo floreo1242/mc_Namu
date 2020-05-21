9c064195379c8571f204008a3087e5faf83e396c
namu <reona7140@naver.com>
2020-05-21 21:41:52 +0900

    fix: message type

--- src/main/java/com/namu/core/rpg/equip/listener/EquipInventoryListener.kt
+++ src/main/java/com/namu/core/rpg/equip/listener/EquipInventoryListener.kt
@@ -1,11 +1,12 @@
 package com.namu.core.rpg.equip.listener
 
+import com.kkomi.devlibrary.extension.sendErrorMessage
 import com.kkomi.devlibrary.nms.getNBTTagCompound
 import com.namu.core.rpg.calculate.model.PlayerStatusRepository
 import com.namu.core.rpg.equip.inventory.EquipInventory
-import com.namu.core.utility.itemdb.model.EquipmentType
 import com.namu.core.rpg.equip.model.PlayerEquipInfoRepository
 import com.namu.core.rpg.level.util.playerLevel
+import com.namu.core.utility.itemdb.model.EquipmentType
 import com.namu.core.utility.itemdb.model.entity.EquipmentOption
 import org.bukkit.Material
 import org.bukkit.entity.Player
@@ -29,31 +30,33 @@ class EquipInventoryListener : Listener {
 
         val player = event.whoClicked as Player
         val cursorItem = event.cursor ?: return
+        val clickedSlot = event.slot
         val equipmentOption = cursorItem.getNBTTagCompound(EquipmentOption::class.java)
 
         // 손에 든 아이템이 장비아이템이 아닐 경우
         if (equipmentOption == null) {
-            player.sendMessage("장비아이템만 장착이 가능합니다.")
+            player.sendErrorMessage("장비아이템만 장착이 가능합니다.")
             event.isCancelled = true
             return
         }
 
         // 반지 인 경우
         val isCorrectEquipmentItem = if (equipmentOption.equipType == EquipmentType.RING) {
-            EquipmentType.RING.index == event.slot || EquipmentType.RING_SUB.index == event.slot
+            EquipmentType.RING.index == clickedSlot || EquipmentType.RING_SUB.index == clickedSlot
         } else { // 반지가 아닌 경우
-            equipmentOption.equipType.index != event.slot
+            equipmentOption.equipType.index != clickedSlot
         }
 
         // 손에 든 장비아이템의 타입이 슬롯이랑 일치하지 않을 경우
         if (isCorrectEquipmentItem) {
+            player.sendErrorMessage("해당 슬롯에는 착용 할 수 없는 아이템 입니다.")
             event.isCancelled = true
             return
         }
 
         // 플레이어의 레벨이 착용 하려는 아이템의 레벨보다 낮을 경우
         if (equipmentOption.levelLimit > player.playerLevel.level) {
-            player.sendMessage("착용하려는 아이템의 레벨이 현재 레벨보다 높습니다.")
+            player.sendErrorMessage("착용하려는 아이템의 레벨이 현재 레벨보다 높습니다.")
             event.isCancelled = true
             return
         }
